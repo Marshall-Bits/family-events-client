ab9a878b3ef015b2123683507f6715dcde11d98a
marshall-bits <kz_@hotmail.es>
2024-03-18 12:04:06 +0100

    adjust adding participant styles

--- src/components/AddParticipantMenu.jsx
+++ src/components/AddParticipantMenu.jsx
@@ -37,7 +37,7 @@ const AddParticipantMenu = ({ availableParticipants, addParticipants }) => {
                     </li>
                 ))}
             </ul>
-            <button onClick={() => handleFadeOut()}>Afegir i tancar</button>
+            <button className='add-and-close-btn' onClick={() => handleFadeOut()}>➕</button>
         </div>
     );
 };
