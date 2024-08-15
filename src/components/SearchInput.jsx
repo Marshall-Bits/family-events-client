f86e3b87cb1c4e23730b3eba7e8339396fa190fb
Marshall-Bits <kz_@hotmail.es>
2024-08-15 17:24:48 +0200

    add margin button to input

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import "./SearchInput.css";
 
 const SearchInput = ({ setDisplayedParticipants, availableParticipants }) => {
   const [searchQuery, setSearchQuery] = useState("");
   return (
     <input
+      className="search-input"
       type="text"
       value={searchQuery}
       onChange={(e) => {
